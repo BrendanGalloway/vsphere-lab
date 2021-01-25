9092d0a74106320a21952ea5ce10b6d5169f522a
Kyle Robbertze <kyle@paddatrapper.com>
2021-01-25 09:11:26 +0200

    fix quoting

--- ibm/setup/ibm_shuttle.sh
+++ ibm/setup/ibm_shuttle.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sops exec-env vpn.enc.env `sshuttle --dns -r $SSH_USER@$SSH_JUMP_HOST $VPN_NETWORK`
+sops exec-env vpn.enc.env 'sshuttle --dns -r $SSH_USER@$SSH_JUMP_HOST $VPN_NETWORK'
