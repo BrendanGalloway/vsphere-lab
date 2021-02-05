2383eff99e9eb1b2affc6e1b48cced30ffe801d4
Brendan Galloway <brendan@flanksource.com>
2021-02-05 09:42:51 +0200

    update nodes to Ubuntu20

--- ibm/setup/build-image.sh
+++ ibm/setup/build-image.sh
@@ -12,11 +12,7 @@ if [[ "$DEBUG" == "true" ]]; then
    verbosity="-vvv"
 fi
 
-UBUNTU_IMAGE=ubuntu1804
-
-if [[ "$VERSION" == *"containerd"* ]]; then
-   UBUNTU_IMAGE=ubuntu2004
-fi
+UBUNTU_IMAGE=ubuntu2004
 
 konfigadm images build $verbosity --image $UBUNTU_IMAGE \
    --resize +15g \
